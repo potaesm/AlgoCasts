cdc05c0b44defc21e17581641f1fc120bea43917
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-03-12 17:15:40 +0700

    EDIT

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,51 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//     let numberArr = [];
+//     for (let i = 0; i <= n; i++) {
+//         if (i > 1) {
+//             numberArr[i] = numberArr[i - 1] + numberArr[i - 2];
+//         } else {
+//             numberArr[i] = i;
+//         }
+//     }
+//     return numberArr[n];
+// }
+
+// function fib(n, numberArr = [], index = 0) {
+//     if (index > n) {
+//         return numberArr[n];
+//     }
+//     if (index > 1) {
+//         numberArr[index] = numberArr[index - 1] + numberArr[index - 2];
+//     } else {
+//         numberArr[index] = index;
+//     }
+//     index++;
+//     return fib(n, numberArr, index);
+// }
+
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (!!cache[args]) {
+            return cache[args];
+        }
+        const result =  fn.apply(this, args);
+        cache[args] = result;
+        console.log(JSON.stringify(cache));
+        return result;
+    }
+}
+
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,113 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// function matrix(n) {
+//     let startColumn = 0;
+//     let startRow = 0;
+//     let endColumn = n - 1;
+//     let endRow = n - 1;
+//     let counter = 1;
+//     let results = [];
+//     for (let x = 0; x < n; x++) {
+//         results[x] = [];
+//     }
+//     while (startColumn <= endColumn && startRow <= endRow) {
+//         for (let i = startColumn; i <= endColumn; i++) {
+//             results[startRow][i] = counter;
+//             counter++;
+//         }
+//         startRow++;
+//         for (let j = startRow; j <= endRow; j++) {
+//             results[j][endColumn] = counter;
+//             counter++;
+//         }
+//         endColumn--;
+//         for (let k = endColumn; k >= startColumn; k--) {
+//             results[endRow][k] = counter;
+//             counter++;
+//         }
+//         endRow--;
+//         for (let l = endRow; l >= startRow; l--) {
+//             results[l][startColumn] = counter;
+//             counter++;
+//         }
+//         startColumn++;
+//     }
+//     return results;
+// }
+
+function matrix(n) {
+    const fillEmptyArray = (length, array = [], index = 0) => {
+        if (index === length) {
+            return array;
+        }
+        array[index] = [];
+        index++;
+        return fillEmptyArray(length, array, index);
+    };
+    const fillSpiralCounter = (results, endColumn, endRow, counter = 1, startColumn = 0, startRow = 0, generalIndex = 0, state = 0) => {
+        if (startColumn > endColumn && startRow > endRow) {
+            return results;
+        }
+
+        /** TOP */
+        if (state === 0) {
+            if (generalIndex > endColumn) {
+                startRow++;
+                generalIndex = startRow;
+                state = 1;
+            } else {
+                results[startRow][generalIndex] = counter;
+                generalIndex++;
+                counter++;
+                return fillSpiralCounter(results, endColumn, endRow, counter, startColumn, startRow, generalIndex, state);
+            }
+        }
+        
+        /** RIGHT */
+        if (state === 1) {
+            if (generalIndex > endRow) {
+                endColumn--;
+                generalIndex = endColumn;
+                state = 2;
+            } else {
+                results[generalIndex][endColumn] = counter;
+                generalIndex++;
+                counter++;
+                return fillSpiralCounter(results, endColumn, endRow, counter, startColumn, startRow, generalIndex, state);
+            }
+        }
+        
+        /** BOTTOM */
+        if (state === 2) {
+            if (generalIndex < startColumn) {
+                endRow--;
+                generalIndex = endRow;
+                state = 3;
+            } else {
+                results[endRow][generalIndex] = counter;
+                generalIndex--;
+                counter++;
+                return fillSpiralCounter(results, endColumn, endRow, counter, startColumn, startRow, generalIndex, state);
+            }
+        }
+        
+        /** LEFT */
+        if (state === 3) {
+            if (generalIndex < startRow) {
+                startColumn++;
+                generalIndex = startColumn;
+                state = 0;
+                return fillSpiralCounter(results, endColumn, endRow, counter, startColumn, startRow, generalIndex, state);
+            } else {
+                results[generalIndex][startColumn] = counter;
+                generalIndex--;
+                counter++;
+                return fillSpiralCounter(results, endColumn, endRow, counter, startColumn, startRow, generalIndex, state);
+            }
+        }
+    };
+    return fillSpiralCounter(fillEmptyArray(n), n - 1, n - 1);
+}
 
 module.exports = matrix;
