8a5d3e74cc1b3c55b6380418baa36d2b7193b103
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-03-14 18:29:11 +0700

    Edit

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,12 +13,15 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+    // insertFirst(data) {
+    //     if (!this.head) {
+    //         this.head = new Node(data);
+    //     } else {
+    //         this.head = new Node(data, this.head);
+    //     }
+    // }
     insertFirst(data) {
-        if (!this.head) {
-            this.head = new Node(data);
-        } else {
-            this.head = new Node(data, this.head);
-        }
+        this.insertAt(data, 0);
     }
     size(node = this.head, n = 0) {
         if (!node) {
@@ -27,14 +30,20 @@ class LinkedList {
         n++;
         return this.size(node.next, n);
     }
+    // getFirst() {
+    //     return this.head;
+    // }
     getFirst() {
-        return this.head;
+        return this.getAt(0);
     }
-    getLast(node = this.head) {
-        if (!!node && !node.next) {
-            return node;
-        }
-        return this.getLast(node.next);
+    // getLast(node = this.head) {
+    //     if (!!node && !node.next) {
+    //         return node;
+    //     }
+    //     return this.getLast(node.next);
+    // }
+    getLast() {
+        return this.getAt(this.size() - 1);
     }
     clear() {
         this.head = null;
@@ -82,6 +91,61 @@ class LinkedList {
         }
         counter++;
         return this.getAt(index, node.next, counter);
+    }
+    removeAt(index, prevNode = null, currentNode = this.head, counter = 0) {
+        if (!currentNode) {
+            return;
+        }
+        if (index === counter) {
+            if (!prevNode) {
+                this.head = currentNode.next;
+                return;
+            }
+            prevNode.next = currentNode.next;
+            return;
+        }
+        counter++;
+        this.removeAt(index, currentNode, currentNode.next, counter);
+    }
+    insertAt(data, index, prevNode = null, currentNode = this.head, counter = 0) {
+        if (!currentNode) {
+            if (!prevNode) {
+                // insertAt 0 of empty linkedList
+                this.head = new Node(data);
+                return;
+            }
+            // insertAt > End
+            prevNode.next = new Node(data);
+            return;
+        }
+        if (index === counter) {
+            if (!prevNode) {
+                // insertAt 0 of non-empty linkedList
+                this.head = new Node(data, currentNode);
+                return;
+            }
+            // insertAt index in the middle
+            prevNode.next = new Node(data, currentNode);
+            return;
+        }
+        counter++;
+        this.insertAt(data, index, currentNode, currentNode.next, counter);
+    }
+    forEach(fn, node = this.head, counter = 0) {
+        if (!!node) {
+            fn(node, counter);
+            if (!!node.next) {
+                counter++;
+                this.forEach(fn, node.next);
+            }
+        }
+    }
+    *[Symbol.iterator]() {
+        let node = this.head;
+        while (!!node) {
+            yield node;
+            node = node.next;
+        }
     }
 }
 
